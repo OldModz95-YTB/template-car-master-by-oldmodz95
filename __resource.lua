ab712b01a971132ca8c52e8b78077c175399b512
OldModz95-YTB <55117264+OldModz95-YTB@users.noreply.github.com>
2020-09-15 11:23:33 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,34 @@
+-- Manifest
+resource_manifest_version 'f15e72ec-3972-4fe4-9c7d-afc5394ae207'
+
+--files {
+	--'data/carcols.meta',
+	--'data/carvariations.meta',
+	--'data/handling.meta',
+	--'data/vehicles.meta',
+   --'data/contentunlockscontentunlocks.meta',
+  -- 'data/vehiclelayouts.meta',
+   -- 'data/shop_vehicle.meta',
+--}
+
+
+--data_file 'CARCOLS_FILE' 'data/carcols.meta'
+--data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+--data_file 'HANDLING_FILE' 'data/handling.meta'
+--data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+
+--data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlockscontentunlocks.meta'
+--data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+--data_file 'VEHICLE_SHOP_DLC_FILE' 'data/shop_vehicle.meta'
+
+
+--client_script { 'client/vehicle_name.lua' }
+
+--►Discord: https://discord.gg/3t2568W
+--►Second discord: https://discord.gg/axZFTQ6
+
+--►Bot Discord: https://discordapp.com/oauth2/authorize?client_id=624463058642796557&scope=bot&permissions=1
+
+--►Twitter: https://twitter.com/oldytb
+
+--►Snap: oldmodz95
---
+++ client/vehicle_name.lua
@@ -0,0 +1,16 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('#GAMENAME_VEHICULE#', 'Exemple nom du véhicule')
+end)
+--►Discord: https://discord.gg/3t2568W
+--►Second discord: https://discord.gg/axZFTQ6
+
+--►Bot Discord: https://discordapp.com/oauth2/authorize?client_id=624463058642796557&scope=bot&permissions=1
+
+--►Twitter: https://twitter.com/oldytb
+
+--►Snap: oldmodz95
